88efe5de0cdbe987a757abc42e1c4ffc717df7be
Rataknak200620 <nimrataknak@gmail.com>
2025-09-08 19:52:30 +0700

    init commit

---
+++ task-service/src/main/java/com/rataknak/taskservice/Controller/TaskController.java
@@ -0,0 +1,32 @@
+package com.rataknak.taskservice.Controller;
+
+import com.rataknak.taskservice.Entity.Task;
+import com.rataknak.taskservice.Repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @GetMapping
+    public List<Task> findAll() {
+        return taskRepository.findAll();
+    }
+
+    @PostMapping
+    public Task createTask(@RequestParam Long userId, @RequestBody Task task) {
+        task.setUserId(userId);
+        return taskRepository.save(task);
+    }
+
+    @GetMapping("/user/{userId}")
+    public List<Task> getTasksByUserId(@PathVariable Long userId) {
+        return taskRepository.findAllByUserId(userId);
+    }
+}
