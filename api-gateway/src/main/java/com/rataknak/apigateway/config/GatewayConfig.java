4750cc924080427a75ee11e1471f948ef5793463
Rataknak200620 <nimrataknak@gmail.com>
2025-09-08 19:52:30 +0700

    init commit

---
+++ api-gateway/src/main/java/com/rataknak/apigateway/config/GatewayConfig.java
@@ -0,0 +1,20 @@
+package com.rataknak.apigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("user-service", r -> r.path("/users/**")
+                        .uri("http://localhost:8081"))
+                .route("task-service", r -> r.path("/tasks/**")
+                        .uri("http://localhost:8082"))
+                .build();
+    }
+}
