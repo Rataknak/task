8ae424eef4cb71f610138ce8fb5113329ea4c781
Rataknak200620 <nimrathanak2006@gmail.com>
2025-10-17 00:00:21 +0700

    updated

---
+++ db-init/init.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE taskdb;
+CREATE DATABASE userdb;
+
+\c userdb;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(255)
+);
+
+\c taskdb;
+
+CREATE TABLE tasks (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    description VARCHAR(255),
+    completed VARCHAR(255),
+    user_id BIGINT
+);
